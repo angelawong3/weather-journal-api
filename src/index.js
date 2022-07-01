9945c0fcff6ef8725e0d0a2f5cbc7a7e57f66395
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-01 14:32:44 +0100

    server with middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 // import express
+const express = require("express");
+
+// declare a PORT heroku or 3001
+const PORT = process.env.PORT || 3001;
 
 // create an app instance using express
+const app = express();
 
-// declare a PORT
+const foo = (req, res, next) => {
+  req.foo = "bar";
+
+  next();
+};
 
 // add middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(foo);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 // listen for requests
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
