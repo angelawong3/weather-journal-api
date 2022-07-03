3190bffa983047a28db2b9cb129c6cacc51ee4e9
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-03 22:47:26 +0100

    connect to db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 // import express
 const express = require("express");
+const mysql2 = require("mysql2");
 
 const routes = require("./routes");
 
 // declare a PORT heroku or 3001
 const PORT = process.env.PORT || 3001;
+
+const config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "weather_journal_db",
+};
+
+// connect to db
+mysql2.createConnection(config);
 
 // create an app instance using express
 const app = express();
