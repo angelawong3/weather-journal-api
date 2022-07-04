347e1d4d6b9d04143d5071dc58a37767295d8ff4
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-04 15:39:40 +0100

    connect to db

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 // import express
 const express = require("express");
-const mysql2 = require("mysql2");
+const mysql2 = require("mysql2/promise");
 
 const routes = require("./routes");
+const dbMiddleware = require("./middleware/db");
 
-// declare a PORT heroku or 3001
-const PORT = process.env.PORT || 3001;
+const init = async () => {
+  try {
+    // declare a PORT heroku or 3001
+    const PORT = process.env.PORT || 3001;
 
-const config = {
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "weather_journal_db",
+    const dbConfig = {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "weather_journal_db",
+    };
+
+    // connect to db
+    const db = await mysql2.createConnection(dbConfig);
+
+    // create an app instance using express
+    const app = express();
+
+    // add middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(dbMiddleware(db));
+    app.use(routes);
+
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to start server | ${error.message}`);
+  }
 };
 
-// connect to db
-mysql2.createConnection(config);
-
-// create an app instance using express
-const app = express();
-
-// add middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-
-// listen for requests
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+init();
